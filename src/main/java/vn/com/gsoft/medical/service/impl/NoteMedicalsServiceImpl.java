6b3a928d23dd7c0869debc74286d55770763d92d
Hải Anh <tysunhb1234@gmail.com>
2024-06-13 11:30:09 +0700

    cập nhật trạng thái phiếu khám

--- src/main/java/vn/com/gsoft/medical/service/impl/NoteMedicalsServiceImpl.java
+++ src/main/java/vn/com/gsoft/medical/service/impl/NoteMedicalsServiceImpl.java
@@ -154,6 +154,25 @@ public class NoteMedicalsServiceImpl extends BaseServiceImpl<NoteMedicals, NoteM
     }
 
     @Override
+    public NoteMedicals changeStatusExam(NoteMedicalsReq objReq) throws Exception {
+        Profile userInfo = this.getLoggedUser();
+        if (userInfo == null)
+            throw new Exception("Bad request.");
+
+        Optional<NoteMedicals> optional = hdrRepo.findById(objReq.getId());
+        if (optional.isEmpty()) {
+            throw new Exception("Không tìm thấy dữ liệu.");
+        } else {
+            if (optional.get().getRecordStatusId() != RecordStatusContains.ACTIVE) {
+                throw new Exception("Không tìm thấy dữ liệu.");
+            }
+        }
+        optional.get().setStatusNote(objReq.getStatusNote());
+        NoteMedicals save = hdrRepo.save(optional.get());
+        return save;
+    }
+
+    @Override
     public NoteMedicals detail(Long id) throws Exception {
         Profile userInfo = this.getLoggedUser();
         if (userInfo == null)
