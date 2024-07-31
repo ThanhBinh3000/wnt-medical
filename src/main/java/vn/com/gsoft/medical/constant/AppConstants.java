1ec1f8feb82356a27bcb499ea15ebd44650b7471
VuxNT <vunt1809@gmail.com>
2024-07-31 13:57:39 +0700

    fix set score

--- src/main/java/vn/com/gsoft/medical/constant/AppConstants.java
+++ src/main/java/vn/com/gsoft/medical/constant/AppConstants.java
@@ -1,9 +1,14 @@
 package vn.com.gsoft.medical.constant;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 public class AppConstants {
     public static Date MinProductionDataDate = new Date(2010-1900, 0, 01);
     public static String ConnectivityStoreID = "CONNECTIVITYDS";
     public static String DictionaryStoreCode = "0012";
+
+    public static BigDecimal EspAmount = BigDecimal.valueOf(0.1);
+
+
 }
